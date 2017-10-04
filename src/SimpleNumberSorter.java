3120521989541700b27dfdf4280c3ef2b9c296b5
AnkitBisain <league@WTS20.attlocal.net>
2017-10-03 19:53:50 -0700

    we did rocket ship and finished simple number sorter

--- src/SimpleNumberSorter.java
+++ src/SimpleNumberSorter.java
@@ -1,40 +1,44 @@
 import javax.swing.JOptionPane;
 
 public class SimpleNumberSorter {
-public static void main(String[] args) {
-String a=	JOptionPane.showInputDialog("Give me one number.");
-String b=	JOptionPane.showInputDialog("Give me another number.");
-String c=	JOptionPane.showInputDialog("Give me one last number.");
-	
-	int aa=Integer.parseInt(a);
-	int bb=Integer.parseInt(b);
-	int cc=Integer.parseInt(c);
-	if(aa>bb&&aa>cc) {
-	System.out.println(aa);
+	public static void main(String[] args) {
+		String a = JOptionPane.showInputDialog("Give me one number.");
+		String b = JOptionPane.showInputDialog("Give me another number.");
+		String c = JOptionPane.showInputDialog("Give me one last number.");
+
+		int aa = Integer.parseInt(a);
+		int bb = Integer.parseInt(b);
+		int cc = Integer.parseInt(c);
+		if (aa < bb && aa < cc) {
+			System.out.println(aa);
+			if (bb < cc) {
+				System.out.println(bb);
+				System.out.println(cc);
+			} else {
+				System.out.println(cc);
+				System.out.println(bb);
+			}
+		} else if (bb < aa && bb < cc) {
+			System.out.println(bb);
+			if (aa < cc) {
+				System.out.println(aa);
+				System.out.println(cc);
+			} else {
+				System.out.println(cc);
+				System.out.println(aa);
+			}
+		}
+
+		else {
+			System.out.println(cc);
+			if (bb < aa) {
+				System.out.println(bb);
+				System.out.println(aa);
+			} else {
+				System.out.println(aa);
+				System.out.println(bb);
+			}
+		}
+
 	}
-	else if (bb) {
-		
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
 }
--- src/methods/RocketShip.java
+++ src/methods/RocketShip.java
@@ -3,25 +3,34 @@
 
 package methods;
 
+import javax.swing.JOptionPane;
+
 public class RocketShip {
 	public static void main(String[] args) {
-		
+
 		// 2. ask the user when to start (JOptionPane.showInputDialog)
 		// 3. convert the user's answer to an int (Integer.parseInt)
-
+		String starting = JOptionPane.showInputDialog("What number do I start to count down with?");
+		int start = Integer.parseInt(starting);
 		// 4. count down from user's starting point
 		// 1. count down from 10 to 0 (print each number)
+		for (int i = start; i > -1; i--) {
+		String blastoff=Integer.toString(i);
+			speak(blastoff);
+
+		}
+		speak("Blast off");
 
 		// 5. when the counting is done, print "blastoff!"
-		// 6. replace the print commands with calls to the speak() method 
+		// 6. replace the print commands with calls to the speak() method
+
 	}
 
-static void speak(String words) {
-   	 try {
-   		 Runtime.getRuntime().exec("say " + words).waitFor();
-   	 } catch (Exception e) {
-   		 e.printStackTrace();
-   	 }
-  }
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
